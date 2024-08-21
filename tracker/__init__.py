240886fee2fcfbc476a30cec1d87b3aceb2ff685
byliu <liubenyuan@gmail.com>
2024-08-21 08:34:30 +0800

    lint

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -3,6 +3,7 @@ multiple target tracking using
 1. alpha-beta-gamma (g-h-k) filter
 2. kalman filter
 """
+
 from .base import Tracker
 from .prototype import Link, Track, GHK
 from .model import model_cv, model_ca, model_dbt, model_ca_visual
